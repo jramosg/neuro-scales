b59edc40b5e4106b59b3d7129ffc078659660ce2
Jon <jonurnieta@gmail.com>
2026-03-06 08:52:01 +0100

    Add English language support to i18n configuration
    Updated the locales in the i18n configuration to include English.
    This change enhances accessibility for a broader audience by
    supporting multiple languages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-    locales: ['es', 'eu'],
+    locales: ['es', 'eu', 'en'],
     defaultLocale: 'es',
     routing: {
       prefixDefaultLocale: true,
